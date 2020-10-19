c9ec2e419d005cb3d097442e2da36f415ae2b9b2
vatsalvj <66792573+vatsalvj@users.noreply.github.com>
2020-10-19 13:35:21 +0530

    Update urls.py
    added item as parameter in pr urls

--- flix/urls.py
+++ flix/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 urlpatterns=[
     path("",views.index,name="index"),
 
-    path("pr/",views.pr,name="pr"),
+    path("pr/<str:item>",views.pr,name="pr"),#added item as parameter
 
     path('login/', auth_views.LoginView.as_view(template_name="login.html")),
 
