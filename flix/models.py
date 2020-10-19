032256cd4955e59b8daeaef2e8518919ef8dafba
vatsalvj <66792573+vatsalvj@users.noreply.github.com>
2020-10-19 13:18:48 +0530

    Update models.py
    Added image field to add the link of the source of image to show in template

--- flix/models.py
+++ flix/models.py
@@ -6,6 +6,7 @@ class Items(models.Model):
     item=models.OneToOneField(null=True,blank=True,on_delete=models.CASCADE)
     name=models.CharField(max_length=100,null=True)
     product=models.CharField(max_length=100,blank=True)
+    img=models.Charfield(max_length=1000,blank=True) #To add the ssource of image to display
 
     def __str__(self):
         return self.item
